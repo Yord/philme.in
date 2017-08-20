ca53ca9e746009da7bb9b5b35b71557a5771ea3f
Philipp Wille <philipp.wille@web.de>
2017-08-20 19:10:04 +0200

    chore (bootstrap): add bootstrap wrapper component

---
+++ src/view/screen/BootstrapStyles/BootstrapStyles.jsx
@@ -0,0 +1,10 @@
+import './bootstrap/fonts/glyphicons-halflings-regular.eot'
+import './bootstrap/fonts/glyphicons-halflings-regular.svg'
+import './bootstrap/fonts/glyphicons-halflings-regular.ttf'
+import './bootstrap/fonts/glyphicons-halflings-regular.woff'
+import './bootstrap/fonts/glyphicons-halflings-regular.woff2'
+import './bootstrap/css/bootstrap.3.3.6.min.css'
+
+export const BootstrapStyles = ({ children }) => children
+
+export default BootstrapStyles
