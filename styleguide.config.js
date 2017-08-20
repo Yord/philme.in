a0e9cb9f87f1587cbfeffea93b949eb776718f8c
Philipp Wille <philipp.wille@web.de>
2017-08-20 19:11:57 +0200

    feat (styleguidist): add bootstrap styles to styleguidist

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  components: 'src/view/presentation/**/[A-Z]*.jsx'
+  components: 'src/view/presentation/**/[A-Z]*.jsx',
+  require: [
+    './src/view/screen/BootstrapStyles/bootstrap/fonts/glyphicons-halflings-regular.eot',
+    './src/view/screen/BootstrapStyles/bootstrap/fonts/glyphicons-halflings-regular.svg',
+    './src/view/screen/BootstrapStyles/bootstrap/fonts/glyphicons-halflings-regular.ttf',
+    './src/view/screen/BootstrapStyles/bootstrap/fonts/glyphicons-halflings-regular.woff',
+    './src/view/screen/BootstrapStyles/bootstrap/fonts/glyphicons-halflings-regular.woff2',
+    './src/view/screen/BootstrapStyles/bootstrap/css/bootstrap.3.3.6.min.css'
+  ]
 }
